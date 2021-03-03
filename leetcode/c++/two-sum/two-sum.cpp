bbb84c027d4112a6bcc769a87ecd495b90e2b96b
saiashish9 <saiashish3760@gmail.com>
2021-03-03 14:13:21 +0530

    problem-statement updated

---
+++ leetcode/c++/two-sum/two-sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc,char const *argv[]){
+
+#ifndef ONLINE_JUDGE
+    freopen("ip.in","r",stdin);
+    freopen("op.in","w",stdout);
+#endif 
+
+   string s;
+   getline(cin,s);
+   cout<<s<<endl;
+
+  return 0;
+}
