08be998c5a6baea0582495ed36a82cd1a104232e
saiashish9 <saiashish3760@gmail.com>
2021-03-07 01:26:48 +0530

    Q3 solved using c++

---
+++ leetcode/c++/3.LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+string longest(string str){
+
+unordered_map<char,int> pos;
+
+int start,st=0,current,max=0;
+
+pos[str[0]]=0;
+
+// start represent's current location where new substring is to be inspected.
+
+// st represent's current location
+
+// current represent's present distance 
+
+int i;
+
+for(i=1;i<str.length();i++){
+   if(pos.find(str[i]) == pos.end()){
+      pos[str[i]]=i;
+      // if it's not existing
+   }else{
+      if(pos[str[i]] >= st){
+        current = i - st;
+        if(max<current){
+          max=current;
+          start=st;
+        }
+        st = pos[str[i]] + 1;   
+      }
+      pos[str[i]] = i;
+   }
+}
+if (max < i - st) {
+  max = i - st;
+  start = st;
+}
+return str.substr(start,max);
+}
+
+int main() {
+  string str="abcdbabsbd";
+  cout<< "Answer :" << endl << longest(str)<<endl;
+  return 0;
+}
