aa02f50e39ccf4e9f40d5ee22d8ff96eafec09a4
saiashish9 <saiashish3760@gmail.com>
2021-03-10 15:35:23 +0530

    Q6  updated

--- leetcode/c++/5.LongestPalindromicString/5.LongestPalindromicString.cpp
+++ leetcode/c++/5.LongestPalindromicString/5.LongestPalindromicString.cpp
@@ -1,10 +1,40 @@
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        
+class Solution{
+  private:
+  int lo,maxLen;
+
+  void extendPalindrome(string s,int j,int k){
+    while (j >= 0 && k < s.length() && s[j] ==  s[k]) {
+		     j--;
+		     k++;
+	  }
+    if(maxLen < k-j-1){
+      lo = j+1;
+      maxLen = k-j-1;
     }
+  } 
+
+
+  public:
+  string longestPalindrome(string s){
+    int len = s.length();
+	  if (len < 2)
+		    return s;
+    for (int i = 0; i < len-1; i++) {
+     	extendPalindrome(s, i, i); 
+     	extendPalindrome(s, i, i+1); 
+    }
+    return s.substr(lo, lo + maxLen);
+  }
+
 };
+
+int main(){
+  Solution s;
+  cout << s.longestPalindrome("bcba") <<endl;
+  return 0;
+}
