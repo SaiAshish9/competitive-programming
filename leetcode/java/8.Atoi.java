84ed12c7054acaf193f1362eb01fa270844de381
saiashish9 <saiashish3760@gmail.com>
2021-03-14 23:58:42 +0530

    atoi updated

---
+++ leetcode/java/8.Atoi.java
@@ -0,0 +1,41 @@
+class ATOI {
+    public static int atoi(String str) {
+        str = str.trim();
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        double result = 0;
+        boolean isNegative = false;
+        int startIndex = 0;
+        if (str.charAt(0) == '+' || str.charAt(0) == '-') {
+            startIndex++;
+        }
+        if (str.charAt(0) == '-') {
+            isNegative = true;
+        }
+        for (int i = startIndex; i < str.length(); i++) {
+            if (str.charAt(i) < '0' || str.charAt(i) > '9') {
+                break;
+            }
+            int digitValue = (int) (str.charAt(i) - '0');
+            result = result * 10 + digitValue;
+        }
+
+        if (isNegative) {
+            result = -result;
+        }
+        if (result > Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        }
+        if (result < Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+
+        return (int) result;
+    }
+
+    public static void main(String... s) {
+        System.out.println(Main.atoi("42"));
+    }
+
+}
